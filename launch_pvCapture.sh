334fb8db061be9512e0a7de3ec440270b0a0a960
Bruce Hill <bhill@slac.stanford.edu>
2019-05-23 15:01:00 -0700

    Reworked how pvs.list is created.

--- launch_pvCapture.sh
+++ launch_pvCapture.sh
@@ -19,9 +19,9 @@ TOP=`readlink -f $(dirname ${BASH_SOURCE[0]})`
 echo PYPROCMGR = $PYPROCMGR
 echo TOP = $TOP
 
-export N_CLIENTS=3
-export N_SERVERS=2
-export N_COUNTERS=8
+export N_CLIENTS=5
+export N_SERVERS=10
+export N_COUNTERS=10
 export N_PVS_PER_CLIENT=$((($N_SERVERS*$N_COUNTERS+$N_CLIENTS-1)/$N_CLIENTS))
 echo N_CLIENTS=$N_CLIENTS
 echo N_SERVERS=$N_SERVERS
@@ -29,33 +29,29 @@ echo N_COUNTERS=$N_COUNTERS
 echo N_PVS_PER_CLIENT=$N_PVS_PER_CLIENT
 
 export CLIENT=pvCapture
-PORT=41000
+PORT=42000
 
 cd $TOP
-declare -a PVS
-PVS=
 
-C=0
 P=0
+C=0
+mkdir -p $TEST_DIR/$CLIENT${C}
+cat /dev/null > $TEST_DIR/$CLIENT${C}/pvs.list
 for (( N = 0; N < $N_COUNTERS ; ++N )) do
 	for (( S = 0; S < $N_SERVERS ; ++S )) do
-		PVS[P]=PVA:GW:TEST:${S}:Count${N}
+		echo PVA:GW:TEST:${S}:Count${N} >> $TEST_DIR/$CLIENT${C}/pvs.list
 		P=$(($P+1))
 		if (( $P >= $N_PVS_PER_CLIENT )) ; then
-			mkdir -p $TEST_DIR/$CLIENT${C}
-			echo ${PVS[*]} > $TEST_DIR/$CLIENT${C}/pvs.list
-			PVS=
 			P=0
 			C=$(($C+1))
+			if (( $C < $N_CLIENTS )) ; then
+				mkdir -p $TEST_DIR/$CLIENT${C}
+				cat /dev/null > $TEST_DIR/$CLIENT${C}/pvs.list
+			fi
 		fi
 	done
 done
-if (( $P > 0 )) ; then
-	mkdir -p $TEST_DIR/$CLIENT${C}
-	echo -e ${PVS[*]// /\\n} > $TEST_DIR/$CLIENT${C}/pvs.list
-	#echo client $C Length PVS=${#PVS[*]}
-	#echo client $C PVS=${PVS[*]}
-fi
-$PYPROCMGR -c $N_CLIENTS -n $CLIENT -p $PORT -D $TEST_DIR bin/$EPICS_HOST_ARCH/pvCapture '-f $TEST_DIR/$CLIENT\$PYPROC_ID/pvs.list'
+#echo $PYPROCMGR -c $N_CLIENTS ...
+$PYPROCMGR -c $N_CLIENTS -n $CLIENT -p $PORT -D $TEST_DIR 'bin/$EPICS_HOST_ARCH/pvCapture -D $TEST_DIR -f $TEST_DIR/$CLIENT$PYPROC_ID/pvs.list'
 
 
