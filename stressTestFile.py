70413cfd9fd8cc97ab35b1e1cb9b6bacb2be26a3
Bruce Hill <bhill@slac.stanford.edu>
2019-07-11 21:40:03 -0700

    Additional support for parsing timecodes and getArray output.

--- stressTestFile.py
+++ stressTestFile.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 import datetime
+import fileinput
+import io
 import os
 import re
 import json
@@ -15,6 +17,29 @@ def fileGetNumLines( filePath ):
         for line in f:
             numLines += 1
     return numLines
+
+def fixPVCaptureFile( filePath ):
+    fileLines = []
+    with open( filePath, mode='r' ) as fd:
+        for line in fd:
+            fileLines.append( line )
+    if len(fileLines) < 1:
+        return
+    lastLine = fileLines[-1]
+    if lastLine.endswith(',\n'):
+        fileLines[-1] = lastLine[0:-2] + "\n"
+        fileLines.append( "]\n" )
+
+        print( "Corrupted file: %s" % ( filePath + ".bak" ) )
+        #print( "Saving  corrupted  file as: %s\n" % ( filePath + ".bak" ) )
+        #print( "Successfully restored file: %s\n" % ( filePath ) )
+        os.rename( filePath, filePath + ".bak" )
+        with open( filePath, mode="w" ) as fw:
+            for line in fileLines:
+                fw.write( line )
+            fw.close()
+            print( "Restored  file: %s" % ( filePath ) )
+            #print( "Successfully restored file: %s\n" % ( filePath ) )
 
 def readPVCaptureFile( filePath ):
     '''Capture files should follow json syntax and contain
@@ -32,7 +57,15 @@ def readPVCaptureFile( filePath ):
         contents = json.load( f )
         return contents
     except BaseException as e:
-        raise InvalidStressTestCaptureFile( "readPVCaptureFile Error: %s: %s" % ( filePath, e ) )
+        f.close()
+        # Try to fix the most common json errors in PVCapture files
+        fixPVCaptureFile( filePath )
+        try:
+            f2 = open( filePath )
+            contents = json.load( f2 )
+            return contents
+        except BaseException as e:
+            raise InvalidStressTestCaptureFile( "readPVCaptureFile Error: %s: %s" % ( filePath, e ) )
 
 def readpvgetFile( filePath ):
     '''pvget files are a temporary hack while pvGet app is not ready.
@@ -67,29 +100,43 @@ def readpvgetFile( filePath ):
     dateRawEx   = r"(\d{4}-\d{2}-\d{2})"
     floatRawEx  = r"(\d+\.?\d*)"
     timeRawEx   = r"(\d{2}:\d{2}:\d+\.?\d*)"
+
     pvgetRawEx  = pvNameRawEx
     pvgetRawEx  = pvgetRawEx + r"\s+" + dateRawEx
     pvgetRawEx  = pvgetRawEx + r"\s+" + timeRawEx
     pvgetRawEx  = pvgetRawEx + r"\s+" + floatRawEx
     pvgetRegEx  = re.compile( pvgetRawEx )
+
+    pvgetRawTsEx  = r"Get: \S{3}\s+(.*)$"
+    pvgetTsRegEx  = re.compile( pvgetRawTsEx )
     #pvgetRawEx = r"\S*\s*(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d+\.?\d*)\s\+(\d+\.?\d*)"
     epicsEpoch  = datetime.datetime( 1990, 1, 1 )
     try:
-        contents = []
+        contents = [[]]
+        pvName =  os.path.splitext( os.path.split[1] )
         with open(filePath, 'r') as f:
             for line in f:
                 if line.startswith( 'Timeout' ):
-                    contents += [ TS_VALUE_TIMEOUT ]
+                    contents[pvName] += [ TS_VALUE_TIMEOUT ]
                     continue
                 match = re.match( r"Read\s+\d+", line )
                 if match:
-                    # TODO: Track # of bytes read w/ ts
+                    # TODO: pvgetArray needs to track # of bytes read w/ ts
                     continue
-                # Use regex and allow pvName to be optional
+
+                # Some PVA pv's just provide one timestamp on Get: line
+                # Get: AAA bbb dd HH:MM:SS zzz YYYY
+                match = pvgetRegTsEx.match( line )
+                if match:
+                    timestamp = datetime.strptime( match.group(1), "%b %d %H:%M:%S %z %Y" )
+                    if timestamp:
+                        lastTs = timestamp
+                    continue
+
+                # TODO: Use regex and allow pvName to be optional
+                # PV:NAME  YYYY-MM-DD HH:MM:SS.FFF   FLOAT
                 match = pvgetRegEx.match( line )
-                if not match:
-                    continue
-                try:
+                if match:
                     # TODO: 3.7 adds datetime.fromisoformat
                     ( YY, MM, DD ) = match.group(2).split('-')
                     ( hh, mm, ss ) = match.group(3).split(':')
@@ -104,11 +151,9 @@ def readpvgetFile( filePath ):
                     tsEpochSec = int(tsEpoch)
                     nSec = int( (tsEpoch - tsEpochSec) * 1e9 )
                     tsValue = [ [ tsEpochSec, nSec ], value ]
-                    contents += [ tsValue ]
-                    continue
-
-                except BaseException as e:
-                    print( "readpvgetFile Error: %s: %s" % ( filePath, e ) )
+                    if match.group(1):
+                        pvName = match.group(1)
+                    contents[pvName] += [ tsValue ]
                     continue
         return contents
     except BaseException as e:
@@ -167,11 +212,13 @@ class stressTestFilePVGet( stressTestFile ):
 
     def processPVGetFile( self, pathTopToFile ):
         try:
-            tsValueList = {}
+            #tsValueList = {}
             self._tsTimeouts = {}
             self._numTimeouts = 0
             priorTs = None
             contents = readpvgetFile( pathTopToFile )
+            #for pvName in contents:
+            #for tsValue in contents[pvName]:
             for tsValue in contents:
                 timeStamp = tsValue[0]  # timeStamp should be [ secPastEpoch, nsec ]
                 value = tsValue[1]
@@ -230,10 +277,40 @@ class stressTestFilePVCapture( stressTestFile ):
 class stressTestFilePVGetArray( stressTestFile ):
     def __init__( self, pathTopToFile ):
         super( stressTestFile, self ).__init__( pathTopToFile )
+        self.processPVGetArrayFile( pathTopToFile )
     def getFileType( self ):
         return "pvgetarray"
-    #def getTsValues( self ):
-    #   return super( stressTestFile, self ).getTsValues()
+
+    def processPVGetArrayFile( self, pathTopToFile ):
+        try:
+            #tsValueList = {}
+            self._tsTimeouts = {}
+            self._numTimeouts = 0
+            priorTs = None
+            return
+            contents = readpvgetArrayFile( pathTopToFile )
+            for tsValue in contents:
+                timeStamp = tsValue[0]  # timeStamp should be [ secPastEpoch, nsec ]
+                value = tsValue[1]
+                if value is None:
+                    self._numTimeouts = self._numTimeouts + 1
+                    if timeStamp is None or timeStamp[0] is None or timeStamp[1] is None:
+                        # Hack till I get timestamped timeouts in *.pvget
+                        if priorTs is not None:
+                            timeStamp = [ priorTs[0] + 2, priorTs[1] + 1 ]
+
+                if timeStamp[0] is not None and timeStamp[1] is not None:
+                    self._tsValues[ timeStamp[0] + timeStamp[1]*1e-9 ] = value
+                    priorTs = timeStamp
+
+        except InvalidStressTestCaptureFile as e:
+            print( e )
+            raise
+            #pass
+        except BaseException as e:
+            print( "processPVGetFile Error: %s: %s" % ( pathTopToFile, e ) )
+            raise
+            #pass
 
 class stressTestFileGWStats( stressTestFile ):
     def __init__( self, pathTopToFile ):
